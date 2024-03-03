4923903e910da6e6cbeaacd847f7aa8ce8ad0af9
Rasmi Jati <jatirasmi65@gmail.com>
2024-03-03 18:36:05 +0545

    Add project files.

---
+++ Controllers/AdditionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionApp.Controllers
+{
+    public class AdditionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //action method
+        [HttpPost]
+        public IActionResult Index(IFormCollection obj)   //method overloading
+        {
+            int num1 = Convert.ToInt32(obj["txtnum1"].ToString());
+            int num2 = Convert.ToInt32(obj["txtnum2"].ToString());
+            int num3 = num1 + num2;
+            ViewBag.data = "Result is : "+num3;
+            return View();
+        }
+     }
+}
